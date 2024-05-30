59401c9fa66271307e9d960677ca7601ce377437
Nick Valdez <nicholastvaldez@gmail.com>
2024-05-30 12:45:11 -0500

    initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nicholas Valdez on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
